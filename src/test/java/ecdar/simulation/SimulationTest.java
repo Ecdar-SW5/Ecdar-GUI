2d67f2d35141bc5508709ea801873ec0e951de5d
APaludan <andreashp@outlook.com>
2022-11-17 21:35:41 +0100

    gg

--- src/test/java/ecdar/simulation/SimulationTest.java
+++ src/test/java/ecdar/simulation/SimulationTest.java
@@ -31,69 +31,69 @@ public class SimulationTest extends TestFXBase {
     public GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
     private final String serverName = InProcessServerBuilder.generateName();
 
-    // @Test
-    // public void testGetInitialStateHighlightsTheInitialLocation() {
-    //     final List<Component> components = generateComponentsWithInitialLocations();
+    @Test
+    public void testGetInitialStateHighlightsTheInitialLocation() {
+        final List<Component> components = generateComponentsWithInitialLocations();
 
-    //     BindableService testService = new EcdarBackendGrpc.EcdarBackendImplBase() {
-    //         @Override
-    //         public void startSimulation(QueryProtos.SimulationStartRequest request,
-    //                                     StreamObserver<QueryProtos.SimulationStepResponse> responseObserver) {
-    //             try {
-    //                 ObjectProtos.LocationTuple locations = LocationTuple.newBuilder().addAllLocations(components.stream()
-    //                         .map(c -> ObjectProtos.Location.newBuilder()
-    //                         .setSpecificComponent(SpecificComponent.newBuilder()
-    //                                 .setComponentName(c.getName()))
-    //                         .setId(c.getInitialLocation().getId())
-    //                         .build())
-    //                         .collect(Collectors.toList()))
-    //                     .build();
+        BindableService testService = new EcdarBackendGrpc.EcdarBackendImplBase() {
+            @Override
+            public void startSimulation(QueryProtos.SimulationStartRequest request,
+                                        StreamObserver<QueryProtos.SimulationStepResponse> responseObserver) {
+                try {
+                    ObjectProtos.LocationTuple locations = LocationTuple.newBuilder().addAllLocations(components.stream()
+                            .map(c -> ObjectProtos.Location.newBuilder()
+                            .setSpecificComponent(SpecificComponent.newBuilder()
+                                    .setComponentName(c.getName()))
+                            .setId(c.getInitialLocation().getId())
+                            .build())
+                            .collect(Collectors.toList()))
+                        .build();
                     
-    //                 ObjectProtos.State state = ObjectProtos.State.newBuilder().setLocationTuple(locations).build();
-    //                 DecisionPoint decisionPoint = DecisionPoint.newBuilder().setSource(state).build();
-    //                 QueryProtos.SimulationStepResponse response = QueryProtos.SimulationStepResponse.newBuilder()
-    //                         .setNewDecisionPoint(decisionPoint)
-    //                         .build();
-    //                 responseObserver.onNext(response);
-    //                 responseObserver.onCompleted();
-    //             } catch (Throwable e) {
-    //                 responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asException());
-    //             }
-    //         }
+                    ObjectProtos.State state = ObjectProtos.State.newBuilder().setLocationTuple(locations).build();
+                    DecisionPoint decisionPoint = DecisionPoint.newBuilder().setSource(state).build();
+                    QueryProtos.SimulationStepResponse response = QueryProtos.SimulationStepResponse.newBuilder()
+                            .setNewDecisionPoint(decisionPoint)
+                            .build();
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                } catch (Throwable e) {
+                    responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asException());
+                }
+            }
 
-    //         @Override
-    //         public void takeSimulationStep(EcdarProtoBuf.QueryProtos.SimulationStepRequest request,
-    //                                        io.grpc.stub.StreamObserver<EcdarProtoBuf.QueryProtos.SimulationStepResponse> responseObserver) {
-    //         }
-    //     };
+            @Override
+            public void takeSimulationStep(EcdarProtoBuf.QueryProtos.SimulationStepRequest request,
+                                           io.grpc.stub.StreamObserver<EcdarProtoBuf.QueryProtos.SimulationStepResponse> responseObserver) {
+            }
+        };
 
-    //     final ManagedChannel channel;
-    //     final EcdarBackendGrpc.EcdarBackendBlockingStub stub;
-    //     try {
-    //         grpcCleanup.register(InProcessServerBuilder
-    //                 .forName(serverName).directExecutor().addService(testService).build().start());
-    //         channel = grpcCleanup.register(InProcessChannelBuilder
-    //                 .forName(serverName).directExecutor().build());
-    //         stub = EcdarBackendGrpc.newBlockingStub(channel);
+        final ManagedChannel channel;
+        final EcdarBackendGrpc.EcdarBackendBlockingStub stub;
+        try {
+            grpcCleanup.register(InProcessServerBuilder
+                    .forName(serverName).directExecutor().addService(testService).build().start());
+            channel = grpcCleanup.register(InProcessChannelBuilder
+                    .forName(serverName).directExecutor().build());
+            stub = EcdarBackendGrpc.newBlockingStub(channel);
 
-    //         QueryProtos.SimulationStartRequest request = QueryProtos.SimulationStartRequest.newBuilder().build();
+            QueryProtos.SimulationStartRequest request = QueryProtos.SimulationStartRequest.newBuilder().build();
 
-    //         var expectedResponse = new ObjectProtos.Location[components.size()];
+            var expectedResponse = new ObjectProtos.Location[components.size()];
 
-    //         for (int i = 0; i < components.size(); i++) {
-    //             Component comp = components.get(i);
-    //             expectedResponse[i] = ObjectProtos.Location.newBuilder()
-    //                     .setSpecificComponent(SpecificComponent.newBuilder().setComponentName(comp.getName()))
-    //                     .setId(comp.getInitialLocation().getId()).build();
-    //         }
+            for (int i = 0; i < components.size(); i++) {
+                Component comp = components.get(i);
+                expectedResponse[i] = ObjectProtos.Location.newBuilder()
+                        .setSpecificComponent(SpecificComponent.newBuilder().setComponentName(comp.getName()))
+                        .setId(comp.getInitialLocation().getId()).build();
+            }
 
-    //         var result = stub.startSimulation(request).getNewDecisionPoint().getSource().getLocationTuple().getLocationsList().toArray();
+            var result = stub.startSimulation(request).getNewDecisionPoint().getSource().getLocationTuple().getLocationsList().toArray();
 
-    //         Assertions.assertArrayEquals(expectedResponse, result);
-    //     } catch (IOException e) {
-    //         Assertions.fail("Exception encountered: " + e.getMessage());
-    //     }
-    // }
+            Assertions.assertArrayEquals(expectedResponse, result);
+        } catch (IOException e) {
+            Assertions.fail("Exception encountered: " + e.getMessage());
+        }
+    }
 
     private List<Component> generateComponentsWithInitialLocations() {
         List<Component> comps = new ArrayList<>();
